5765a827b573fcaf95f4a7bf1772ed5ca8043373
sadauchi <kysnez4@gmail.com>
2025-02-06 14:11:55 +0300

    update cod

--- src/processing.py
+++ src/processing.py
@@ -1,37 +1,43 @@
-from typing import Any
+
+# src/widget.py
+from datetime import datetime
+from masks import mask_account, mask_card
 
 
-def filter_by_state(list_of_dicts: list[Any], state: str = "EXECUTED") -> list[Any]:
-    """Проверяет значение в словарях на заданное и если оно совпадает то, выводит его"""
-    correct_list = []
-    for num_dict in range(0, len(list_of_dicts) - 1):
-        if list_of_dicts[num_dict]["state"] == state:
-            correct_list.append(list_of_dicts[num_dict])
-    return correct_list
+def mask_account_card(account_info: str) -> str:
+    """
+    Маскирует номер карты или счета в строке.
+
+    Args:
+        account_info (str): Строка, содержащая тип и номер карты или счета.
+            Примеры: "Visa Platinum 7000792289606361", "Maestro 7000792289606361", "Счет 73654108430135874305"
+
+    Returns:
+        str: Строка с замаскированным номером.
+    """
+    parts = account_info.split()
+    if not parts:
+        return account_info  # Если строка пустая, возвращаем ее без изменений.
+
+    if parts[0].lower() in ("visa", "maestro"):
+        return f"{parts[0]} {mask_card(''.join(parts[1:]))}"
+    elif parts[0].lower() == "счет":
+        return f"{parts[0]} {mask_account(''.join(parts[1:]))}"
+    else:
+        return account_info  # Если тип не распознан, возвращаем исходную строку
 
 
-def sort_by_date(list_of_dicts: list[Any], sorting_direct: bool = True) -> list[Any]:
-    """Сортирует список по датам в словарях по убывание(по умолчанию)"""
-    return sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=sorting_direct)
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку с датой в формате "ГГГГ-ММ-ДДTчч:мм:сс.миллисекунды" в формат "ДД.ММ.ГГГГ".
 
+    Args:
+        date_str (str): Строка с датой в формате "ГГГГ-ММ-ДДTчч:мм:сс.миллисекунды".
+            Пример: "2024-03-11T02:26:18.671407"
 
-print(
-    filter_by_state(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
-print(
-    sort_by_date(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+    Returns:
+        str: Строка с датой в формате "ДД.ММ.ГГГГ".
+        Пример: "11.03.2024"
+    """
+    date_object = datetime.fromisoformat(date_str.replace("Z", "+00:00"))
+    return date_object.strftime("%d.%m.%Y")

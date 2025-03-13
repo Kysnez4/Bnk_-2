3ee0458209a584d62b1b4106e392365289d06644
Kuznetsov.P <kysnez4@gmail.com>
2025-03-13 13:57:13 +0300

    add external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,36 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def calculate_transaction_amount(transaction):
+    """
+    Вычисляет сумму транзакции в рублях.
+    Args:
+        transaction (dict): Словарь, представляющий транзакцию.
+                         Обязательные ключи: 'amount' (float), 'currency' (str).
+    Returns:
+        float: Сумма транзакции в рублях.
+               Возвращает None в случае ошибки.
+    """
+    amount, currency = transaction.get("amount"), transaction.get("currency")
+    if not all([amount, currency]) or not isinstance(amount, (int, float)) or not isinstance(currency, str):
+        print("Ошибка: Некорректные данные транзакции")
+        return None
+    if currency == "RUB":
+        return float(amount)
+
+    api_key = os.getenv("API_KEY")
+    if currency not in ("USD", "EUR"):
+        print(f"Ошибка: Неподдерживаемая валюта: {currency}")
+        return None
+
+    url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
+    # print(url)
+    response = requests.get(url, headers={"apikey": api_key})
+    response.raise_for_status()
+    rate = response.json()["rates"]["RUB"]
+    return float(amount * rate)

bb854919ba30e4fb4d4c72186c94c2b946c12053
Kuznetsov.P <kysnez4@gmail.com>
2025-03-14 16:29:53 +0300

    update

--- src/external_api.py
+++ src/external_api.py
@@ -11,31 +11,40 @@ def calculate_transaction_amount(transaction):
     Вычисляет сумму транзакции в рублях.
     Args:
         transaction (dict): Словарь, представляющий транзакцию.
-                         Обязательные ключи: 'amount' (float), 'currency' (str).
+                         Обязательные ключи: 'amount' (float), 'currency','code' (str).
     Returns:
         float: Сумма транзакции в рублях.
                Возвращает None в случае ошибки.
     """
-    amount, currency = transaction.get("amount"), transaction.get("currency")
-    if not all([amount, currency]) or not isinstance(amount, (int, float)) or not isinstance(currency, str):
-        print("Ошибка: Некорректные данные транзакции")
-        return None
-    if currency == "RUB":
-        return float(amount)
-
-    api_key = os.getenv("API_KEY")
-    if currency not in ("USD", "EUR"):
-        print(f"Ошибка: Неподдерживаемая валюта: {currency}")
-        return None
-
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
-    # print(url)
-    response = requests.get(url, headers={"apikey": api_key})
     try:
+        amount = transaction["amount"]
+        currency = transaction["currency"]["code"]
+
+        if not isinstance(amount, str) or not isinstance(currency, str):
+            print("Ошибка: Некорректные данные транзакции")
+            return None
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            print("Ошибка: Некорректный формат суммы транзакции")
+            return None
+
+        if currency == "RUB":
+            return amount
+
+        api_key = os.getenv("API_KEY")
+        if currency not in ("USD", "EUR"):
+            print(f"Ошибка: Неподдерживаемая валюта: {currency}")
+            return None
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        # print(url)
+        response = requests.get(url, headers={"apikey": api_key})
         response.raise_for_status()
         data = response.json()
         return float(data["result"])
-    except KeyError:
-        print("Ошибка: Неверный формат ответа API")
-        return None
 
+    except (KeyError, TypeError) as e:
+        print(f"Ошибка: Некорректная структура данных транзакции: {e}")
+        return None
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -14,53 +14,140 @@ def mock_env(monkeypatch):
     monkeypatch.delenv("API_KEY", raising=False)
 
 
-def test_calculate_transaction_amount_eur():
-    """Тест для транзакции в EUR с мокированным API."""
+def test_calculate_transaction_amount_usd(mock_env):
+    """Тест для транзакции в USD с мокированным API."""
     api_key = os.getenv("API_KEY")
     with patch("requests.get") as mock_get:
         # Настройка мокированных объектов
         mock_response = mock_get.return_value
         mock_response.raise_for_status.return_value = None  # Имитация успешного ответа
-        mock_response.json.return_value = {"rates": {"RUB": 85.0}}
+        mock_response.json.return_value = {"result": 8221.37 * 90.0}  # Мокируем результат конвертации USD в RUB
 
-        transaction_data = {"amount": 50, "currency": "EUR"}
-        expected_result = None
-        actual_result = calculate_transaction_amount(transaction_data)
+        data = {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+              "amount": "8221.37",
+              "currency": {
+                "name": "USD",
+                "code": "USD"
+              }
+            },
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+          }
+        expected_result = 8221.37 * 90.0  # Ожидаемый результат (сумма в USD * курс)
+        actual_result = calculate_transaction_amount(data["operationAmount"])
 
         assert actual_result == expected_result
         mock_get.assert_called_once_with(
-            "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount=50", headers={"apikey": api_key}
+            "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=8221.37", headers={"apikey": api_key}
         )
 
 
 def test_calculate_transaction_amount_rub(mock_env):
     """Тест для транзакции в RUB."""
-    transaction_data = {"amount": 200, "currency": "RUB"}
-    expected_result = 200.0
-    actual_result = calculate_transaction_amount(transaction_data)
+    data = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "8221.37",
+          "currency": {
+            "name": "RUB",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    expected_result = float(8221.37)
+    actual_result = calculate_transaction_amount(data["operationAmount"])
 
     assert actual_result == expected_result
 
 
 def test_calculate_transaction_amount_invalid_currency(mock_env):
     """Тест для неподдерживаемой валюты."""
-    transaction_data = {"amount": 100, "currency": "GBP"}
-    actual_result = calculate_transaction_amount(transaction_data)
+    data = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "8221.37",
+          "currency": {
+            "name": "GBP",
+            "code": "GBP"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    actual_result = calculate_transaction_amount(data["operationAmount"])
 
     assert actual_result is None
 
 
 def test_calculate_transaction_amount_missing_data(mock_env):
     """Тест для отсутствующих данных в транзакции."""
-    transaction_data = {"amount": 100}
-    actual_result = calculate_transaction_amount(transaction_data)
+    data = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "8221.37"
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    actual_result = calculate_transaction_amount(data["operationAmount"])
 
     assert actual_result is None
 
 
+def test_calculate_transaction_amount_invalid_type(mock_env):
+    """Тест для некорректного типа данных суммы транзакции."""
+    data = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "abc",
+          "currency": {
+            "name": "USD",
+            "code": 33
+          }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    actual_result = calculate_transaction_amount(data["operationAmount"])
+
+    assert actual_result is None
+
 def test_calculate_transaction_amount_invalid_amount_type(mock_env):
     """Тест для некорректного типа данных суммы транзакции."""
-    transaction_data = {"amount": "abc", "currency": "USD"}
-    actual_result = calculate_transaction_amount(transaction_data)
+    data = {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "abc",
+          "currency": {
+            "name": "USD",
+            "code": "USD"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    actual_result = calculate_transaction_amount(data["operationAmount"])
 
     assert actual_result is None
